8a362d83f7a7a6f2625a1222308e032f6ea3d279
Ronnie Nyaga <ronnienyaga@gmail.com>
2016-03-29 17:41:15 +0300

    Refactored code

--- main.js
+++ main.js
@@ -15,34 +15,36 @@ const url = require('url');
 //Include fs module, which is helpful in reading and writing files.
 const fs = require("fs");
 
-// Create the server. Function passed as parameter is called on every request made.
-// request variable holds all request parameters
-// response variable allows you to do anything with response sent to the client.
-const server = http.createServer((request, response) => {
-  // Parse the request for arguments and store them in _get variable.
+function onRequest(request,response){
+	// Parse the request for arguments and store them in _get variable.
 	// This function parses the url from request and returns object representation.
 	var _get = url.parse(request.url, true).query;
 
-  response.writeHead(200,{'Content-Type': 'text/html'});
-  // Send data and end response.
-  var output =  '<!DOCTYPE html>' +
-                '<html lang="en">'+
-                  '<head>'+
-                    '<title>NodeJS 101</title>'+
-                  '</head>'+
-                  '<body>'+
-                    '<h3>Hello World!!</h3>'+
-                    '<label>Name:</label>' + '<span style="margin-left:150px;">' + _get['name'] + '</span><br /><br />'+
-                    '<label>Age:</label>' + '<span style="margin-left:150px;">' + _get['age'] + '</span><br /><br />'+ '<label>Timestamp:</label>' + '<span style="margin-left:100px;">' + Date.now() + '</span><br /><br />'+
-                  '</body>'+
-                '</html>';
-  // Write request parameters to a file
-  fs.appendFile('file.txt', output, 'utf8', (err) => {
-    if (err) throw err;
-    console.log('The data was appended to file!');
-  });
-  response.end(output);
+	response.writeHead(200,{'Content-Type': 'text/html'});
+	// Send data and end response.
+	var output =  '<!DOCTYPE html>' +
+								'<html lang="en">'+
+									'<head>'+
+										'<title>NodeJS 101</title>'+
+									'</head>'+
+									'<body>'+
+										'<h3>Hello World!!</h3>'+
+										'<label>Name:</label>' + '<span style="margin-left:150px;">' + _get['name'] + '</span><br /><br />'+
+										'<label>Age:</label>' + '<span style="margin-left:150px;">' + _get['age'] + '</span><br /><br />'+
+										'<label>Timestamp:</label>' + '<span style="margin-left:100px;">' + Date.now() + '</span><br /><br />'+
+									'</body>'+
+								'</html>';
+	// Write request parameters to a file
+	fs.appendFile('file.txt', output, 'utf8', (err) => {
+		if (err) throw err;
+		console.log('The data was appended to file!');
+	});
+	response.end(output);
+}
 
-}).listen(8080); // Listen on the 8080 port.
+// Create the server. Function passed as parameter is called on every request made.
+// request variable holds all request parameters
+// response variable allows you to do anything with response sent to the client.
+const server = http.createServer(onRequest).listen(8080); // Listen on the 8080 port.
 
 console.log('Server running at http://localhost:8080/');
