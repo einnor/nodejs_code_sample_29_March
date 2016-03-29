a5b5ba0c2372299121b70992e72ce0c8a89a55d3
Ronnie Nyaga <ronnienyaga@gmail.com>
2016-03-29 11:23:27 +0300

    Handled URL parameters

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
+/**
+ * Cytonn Technologies
+ *
+ * @author: Ronnie Nyaga <rnyaga@cytonn.com>
+ *
+ * Project: nodejs.app
+ *
+ */
+
 
 // Include http module.
 var http = require('http');
+// Include url module, which is helpful in parsing request parameters.
+var url = require('url');
 
 // Create the server. Function passed as parameter is called on every request made.
 // request variable holds all request parameters
 // response variable allows you to do anything with response sent to the client.
-var server = http.createServer(function(req, res) {
-  res.writeHead(200,{'Content-Type': 'text/plain'});
-  res.end('Hello Http');
-}).listen(8080);
+var server = http.createServer(function(request, response) {
+  // Parse the request for arguments and store them in _get variable.
+	// This function parses the url from request and returns object representation.
+	var _get = url.parse(request.url, true).query;
+
+  response.writeHead(200,{'Content-Type': 'text/plain'});
+  // Send data and end response.
+  response.end('Hello World\n\nName:\t\t' + _get['name'] + '\n\nAge:\t\t' + _get['age']);
+
+}).listen(8080); // Listen on the 8080 port.
