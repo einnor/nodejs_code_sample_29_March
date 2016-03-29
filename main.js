ea4d56be2578211ada1f6e010ef8b4a59fe03347
Ronnie Nyaga <ronnienyaga@gmail.com>
2016-03-29 14:49:48 +0300

    Added HTML elements

--- main.js
+++ main.js
@@ -23,9 +23,19 @@ const server = http.createServer((request, response) => {
 	// This function parses the url from request and returns object representation.
 	var _get = url.parse(request.url, true).query;
 
-  response.writeHead(200,{'Content-Type': 'text/plain'});
+  response.writeHead(200,{'Content-Type': 'text/html'});
   // Send data and end response.
-  var output = '\n\n\n\n\t\t\t\tHello World!!\n\n\t\t\t\tName:\t\t' + _get['name'] + '\n\n\t\t\t\tAge:\t\t' + _get['age'];
+  var output =  '<!DOCTYPE html>' +
+                '<html lang="en">'+
+                  '<head>'+
+                    '<title>NodeJS 101</title>'+
+                  '</head>'+
+                  '<body>'+
+                    '<h3>Hello World!!</h3>'+
+                    '<label>Name:</label>' + '<span style="margin-left:150px;">' + _get['name'] + '</span><br /><br />'+
+                    '<label>Age:</label>' + '<span style="margin-left:150px;">' + _get['age'] + '</span><br /><br />'+ '<label>Timestamp:</label>' + '<span style="margin-left:100px;">' + Date.now() + '</span><br /><br />'+
+                  '</body>'+
+                '</html>';
   // Write request parameters to a file
   fs.appendFile('file.txt', output, 'utf8', (err) => {
     if (err) throw err;
